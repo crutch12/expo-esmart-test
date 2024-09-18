ebbd60860cb283d1ced67e7951e9b573b44946a1
Konstantin Barabanov <criitch@yandex.ru>
2024-09-18 15:42:19 +0300

    chore: prebuild android

--- App.js
+++ App.js
@@ -1,6 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import SQLite from 'react-native-sqlite-2'
+
+const db = SQLite.openDatabase('test.db', '1.0', '', 1)
+
+console.log(db)
+
 export default function App() {
   return (
     <View style={styles.container}>
