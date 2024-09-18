cb29882d9cac779a57463ab020802ade62baa7a0
Konstantin Barabanov <criitch@yandex.ru>
2024-09-18 17:32:24 +0300

    @WIP

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { multiply } from 'react-native-esmart';
+import { useQuery, QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import SQLite from 'react-native-sqlite-2'
 
@@ -8,15 +9,30 @@ const db = SQLite.openDatabase('test.db', '1.0', '', 1)
 
 console.log(db)
 
-export default function App() {
-  const value = multiply(2, 3)
+// Create a client
+const queryClient = new QueryClient()
+
+function Main() {
+  const { data, error, isLoading } = useQuery({
+    queryFn: () => multiply(2, 3),
+    queryKey: [],
+  })
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <Text>value - {value}</Text>
+      {isLoading && <Text>isLoading - true</Text>}
+      {data && <Text>data - {data}</Text>}
+      {error && <Text>error - {error.message}</Text>}
       <StatusBar style="auto" />
     </View>
   );
+}
+
+export default function App() {
+  return <QueryClientProvider client={queryClient}>
+      <Main />
+  </QueryClientProvider>
 }
 
 const styles = StyleSheet.create({
