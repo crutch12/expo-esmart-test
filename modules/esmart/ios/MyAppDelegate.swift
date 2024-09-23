2c373d03f102727eb8d023ae925ee614dd69abdb
Konstantin Barabanov <criitch@yandex.ru>
2024-09-23 11:29:08 +0300

    feat: ios - первые попытки подписки на AppDelegate

--- modules/esmart/ios/MyAppDelegate.swift
+++ modules/esmart/ios/MyAppDelegate.swift
@@ -1,5 +1,6 @@
 import ExpoModulesCore
 
+// https://docs.expo.dev/modules/appdelegate-subscribers/
 public class MyAppDelegate: ExpoAppDelegateSubscriber {
   public func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
     // 3.3.7
@@ -20,5 +21,33 @@ public class MyAppDelegate: ExpoAppDelegateSubscriber {
     BLEAdvertiser.enableSendUserId()
 
     return true
+  }
+
+  // "required" is required
+  public override required init() {
+    super.init()
+
+    // @NOTE: Не создаём подписку на NotificationCenter.default.addObserver, т.к. методы класса AppDelegate и так реализуют эту подписку
+    // к тому же в оригинале используется, зачем-то, обе реализации, что странно
+    // https://www.hackingwithswift.com/example-code/system/how-to-detect-when-your-app-moves-to-the-background
+  }
+
+//   deinit {
+//     NotificationCenter.default.removeObserver(self)
+//   }
+
+  public func applicationDidEnterBackground(_ application: UIApplication) {
+    // The app is now in the background.
+    libKeyCard.hostAppDidEnterBackground(application)
+  }
+
+  public func applicationDidBecomeActive(_ application: UIApplication) {
+    // The app has become active.
+    libKeyCard.hostAppDidBecomeActive(application)
+  }
+
+  public func applicationWillTerminate(_ application: UIApplication) {
+    // The app is about to terminate.
+    libKeyCard.hostAppDidWillTerminate(application)
   }
 }
